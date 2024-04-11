a074c3419133a3cfb0a425e19c5fc9e26dd0cb74
iannjau <lantanacreations@gmail.com>
2024-04-11 12:31:48 +0300

    add my project

--- index.js
+++ index.js
@@ -1,47 +1,30 @@
-let URL = "https://api.artic.edu/api/v1/artworks/search?q=cats"
-var navbarShrink = function () {
-    const navbarCollapsible = document.body.querySelector('#mainNav');
-    if (!navbarCollapsible) {
-        return;
-    }
-    if (window.scrollY === 0) {
-        navbarCollapsible.classList.remove('navbar-shrink')
-    } else {
-        navbarCollapsible.classList.add('navbar-shrink')
-    }
+let URL = "https://api.artic.edu/api/v1/artworks/search?q=cats";
+function renderOneImages(data) {
+  let card = document.createElement("li");
+  card.className = "card";
+  card.innerHTML = `
+ <img src="${data.thumbnail.lqip}" alt="A bronze lion, deep green and muscular, looks out in the distance from its pedestal in front of the Art Institute of Chicago." width="8430" height="5620">
+ <div class="content">
+    <h4>${data.title}</h4>
+    <p>${data.alt_text}</p>
+    
+ </div>
+ `;
+  document.querySelector("#data-list").appendChild(card);
+}
 
-};
-
-// Shrink the navbar 
-navbarShrink();
-
-// Shrink the navbar when page is scrolled
-document.addEventListener('scroll', navbarShrink);
-
-// Activate scrollspy on the main nav element
-const mainNav = document.body.querySelector('#mainNav');
-if (mainNav) {
-    new bootstrap.ScrollSpy(document.body, {
-        target: '#mainNav',
-        rootMargin: '0px 0px -40%',
+function getAllImages() {
+  fetch("http://localhost:3000/data")
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((data) => renderOneImages(data));
     });
-};
+  console.log("before fetch returns");
+}
 
-// Collapse responsive navbar when toggler is visible
-const navbarToggler = document.body.querySelector('.navbar-toggler');
-const responsiveNavItems = [].slice.call(
-    document.querySelectorAll('#navbarResponsive .nav-link')
-);
-responsiveNavItems.map(function (responsiveNavItem) {
-    responsiveNavItem.addEventListener('click', () => {
-        if (window.getComputedStyle(navbarToggler).display !== 'none') {
-            navbarToggler.click();
-        }
-    });
-});
-
-// Activate SimpleLightbox plugin for portfolio items
-new SimpleLightbox({
-    elements: '#portfolio a.portfolio-box'
-});
-
+function intialize() {
+  //     getimages()
+  //     console.log('after get images')
+  getAllImages();
+}
+intialize();
